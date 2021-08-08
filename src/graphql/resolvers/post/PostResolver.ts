eed4ed271716e1609af168e0d2d8624ded3cf04f
Omkar <omkarak45@gmail.com>
2021-08-08 23:38:38 +0530

    improved createPostMutation

--- src/graphql/resolvers/post/PostResolver.ts
+++ src/graphql/resolvers/post/PostResolver.ts
@@ -1,30 +1,64 @@
 import { Post } from '@prisma/client'
+import { z } from 'zod'
 import { upload } from '../../../lib/upload'
 import { builder } from '../../builder'
+import { HashTag, parseHashtags } from '../../utils/hashtags'
 // upload photo with caption
 
 const CreatePostResponse = builder.objectRef<Post>('Post')
 
 CreatePostResponse.implement({
 	fields: (t) => ({
-		body: t.exposeString('body', { nullable: true }),
+		caption: t.exposeString('caption', { nullable: true }),
+		image: t.exposeString('image', { nullable: true }),
 	}),
 })
 
 const CreatePostInput = builder.inputType('CreatePostInput', {
 	fields: (t) => ({
+		caption: t.field({
+			type: 'String',
+			validate: { schema: z.string().min(1).max(256) },
+		}),
 		media: t.field({ type: 'FileUpload' }),
+		// add caption here
 	}),
 })
 
 builder.mutationField('createPost', (t) =>
 	t.field({
-		type: 'String',
+		type: CreatePostResponse,
+
 		args: { input: t.arg({ type: CreatePostInput }) },
+
+		authScopes: {
+			user: true,
+		},
+
 		resolve: async (_, { input }, { prisma, user }) => {
+			/**  Incoming image file  */
 			const media = await input.media
 			const response = await upload(media)
-			return response.url
+
+			/** Parse hashtags from caption */
+			let hashTags: Array<HashTag> = []
+
+			if (input.caption) {
+				hashTags = parseHashtags(input.caption)
+			}
+
+			return await prisma.post.create({
+				data: {
+					caption: input.caption,
+					image: response.url,
+					user: { connect: { id: user!.id } },
+					...(hashTags.length > 0 && {
+						hashtags: {
+							connectOrCreate: hashTags,
+						},
+					}),
+				},
+			})
 		},
 	})
 )
--- src/graphql/utils/hashtags.ts
+++ src/graphql/utils/hashtags.ts
@@ -1,4 +1,13 @@
-export function parseHashtags(caption: string) {
+export interface HashTag {
+	where: {
+		hashtag: string
+	}
+	create: {
+		hashtag: string
+	}
+}
+
+export function parseHashtags(caption: string): Array<HashTag> {
 	const hashtags = caption.match(/#[\w]+/g) || []
 	return hashtags.map((hashtag) => ({
 		where: { hashtag },
